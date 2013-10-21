4cff20736284afd1e51bbb6ab1afa1b995806129
Charles Lavery <charles.lavery@gmail.com>
2013-10-20 22:31:34 -0400

    more keybinds

--- keybindings.el
+++ keybindings.el
@@ -1,4 +1,6 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (define-key global-map (kbd "\C-z") 'evil-emacs-state)
 
+(define-key global-map (kbd "\C-x k") 'kill-this-buffer)
+
 (provide 'keybindings)
