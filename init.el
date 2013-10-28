32fa9064f2aea3b3c3ecb36fbb80753cc46cb012
Charles Lavery <clavery@pixelmedia.com>
2013-10-28 13:28:27 -0400

    fix ido and add projectile

--- init.el
+++ init.el
@@ -87,6 +87,7 @@
 (setq ido-use-virtual-buffers t)
 (setq ido-everywhere t)
 (setq ido-max-directory-size 100000)
+(ido-everywhere 1)
 
 ; turn off bell
 (setq ring-bell-function 'ignore)
@@ -207,3 +208,19 @@
 (define-key global-map (kbd "C-<return>") 'ace-jump-mode)
 
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
+
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+(require 'flx-ido)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
