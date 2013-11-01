9079e0ab91557573f09a9fd0cca3772bdd4ac630
Charles Lavery <charles.lavery@gmail.com>
2013-10-31 22:12:49 -0400

    smex

--- init.el
+++ init.el
@@ -110,6 +110,10 @@
 
 (require 'smex)
 (smex-initialize)
+(define-key global-map (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 (require 'buffers)
 
