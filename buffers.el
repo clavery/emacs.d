d9ef6a8e20fa74283fbd8b31e5dbcc6eaed28d27
Charles Lavery <clavery@pixelmedia.com>
2013-11-04 13:35:46 -0500

    ibuffer changes

--- buffers.el
+++ buffers.el
@@ -15,10 +15,10 @@
 (setq ibuffer-show-empty-filter-groups nil)
 
 (defun ibuffer-vc-add-vc-filter-groups ()
-      (interactive)
-      (dolist (group (ibuffer-vc-generate-filter-groups-by-vc-root))
-        (add-to-list 'ibuffer-filter-groups group t)))
- 
+  (interactive)
+  (dolist (group (ibuffer-vc-generate-filter-groups-by-vc-root))
+    (add-to-list 'ibuffer-filter-groups group t)))
+
 (add-hook 'ibuffer-hook
           (lambda ()
             ;; (ibuffer-vc-set-filter-groups-by-vc-root)
@@ -38,12 +38,30 @@
         ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
         (t (format "%8d" (buffer-size)))))
 
+     (define-key ibuffer-mode-map (kbd "C-c g") 'ibuffer-vc-set-filter-groups-by-vc-root)
+     (define-key ibuffer-mode-map (kbd "C-c C-g") 'ibuffer-vc-set-filter-groups-by-vc-root)
+
+     (setq ibuffer-formats '((mark modified read-only " "
+                                   (name 18 18 :left :elide)
+                                   " "
+                                   (size-h 9 -1 :right)
+                                   " "
+                                   (mode 16 16 :left :elide)
+                                   " " filename-and-process)
+                             (mark modified read-only vc-status-mini " "
+                                   (name 18 18 :left :elide)
+                                   " "
+                                   (size-h 9 -1 :right)
+                                   " "
+                                   (mode 16 16 :left :elide)
+                                   " "
+                                   (vc-status 16 16 :left)
+                                   " "
+                                   filename-and-process)))
      ))
 
+
+
 (setq ibuffer-filter-group-name-face 'font-lock-doc-face)
-
-(define-key ibuffer-mode-map (kbd "C-c g") 'ibuffer-vc-set-filter-groups-by-vc-root)
-(define-key ibuffer-mode-map (kbd "C-c C-g") 'ibuffer-vc-set-filter-groups-by-vc-root)
-
 
 (provide 'buffers)
